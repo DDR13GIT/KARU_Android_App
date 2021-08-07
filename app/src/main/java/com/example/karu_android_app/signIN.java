6781499e7e1a1b8e29bb3136b42d2363af9cf8c7
Maisha2510 <maisha2510@gmail.com>
2021-08-07 13:30:37 +0600

    Sign in merge with sign up firebase

--- app/src/main/java/com/example/karu_android_app/signIN.java
+++ app/src/main/java/com/example/karu_android_app/signIN.java
@@ -2,13 +2,49 @@ package com.example.karu_android_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class signIN extends AppCompatActivity {
+public class signIN extends AppCompatActivity implements OnClickListener{
+    private EditText signInEmail,signInPass;
+    private TextView signUpTextView;
+    private Button signInBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        this.setTitle("Sign In Activity");
+
+        signInEmail=findViewById(R.id.emailText);
+        signInPass=findViewById(R.id.passwordText);
+
+        signUpTextView=findViewById(R.id.newHere);
+        signInBtn=findViewById(R.id.logInBTN);
+
+        signUpTextView.setOnClickListener(this);
+        signInBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+
+            case R.id.logInBTN:
+
+                break;
+
+            case R.id.newHere:
+                Intent intent = new Intent(getApplicationContext(),signup.class);
+                startActivity(intent);
+
+                break;
+        }
+
     }
 }
