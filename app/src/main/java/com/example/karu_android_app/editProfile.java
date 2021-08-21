d49800b1399e08e79bebdc06db91bb3c73f910cb
DDR13GIT <ddroy13@gmail.com>
2021-08-22 01:27:27 +0600

    some modification done on top of maisha's design

--- app/src/main/java/com/example/karu_android_app/editProfile.java
+++ app/src/main/java/com/example/karu_android_app/editProfile.java
@@ -2,13 +2,23 @@ package com.example.karu_android_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class editProfile extends AppCompatActivity {
-
+private ImageButton back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+        back = findViewById(R.id.backBTN);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),settingFragment.class);
+            }
+        });
     }
 }
