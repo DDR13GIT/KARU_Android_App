8ebc661d169b0c47adda4c75031798616c0d7ccd
DDR13GIT <ddroy13@gmail.com>
2021-09-04 03:12:24 +0600

    permission page design half done

---
+++ app/src/main/java/com/example/karu_android_app/permission_page.java
@@ -0,0 +1,14 @@
+package com.example.karu_android_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class permission_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permission_page);
+    }
+}
