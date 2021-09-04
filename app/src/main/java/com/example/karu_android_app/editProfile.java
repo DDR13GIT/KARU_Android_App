d4f0c5a196b0bc3c312fbe00265eef840175273e
Maisha2510 <maisha2510@gmail.com>
2021-09-05 00:26:54 +0600

    setting page firebase connection

--- app/src/main/java/com/example/karu_android_app/editProfile.java
+++ app/src/main/java/com/example/karu_android_app/editProfile.java
@@ -5,15 +5,60 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class editProfile extends AppCompatActivity {
 private ImageButton back;
+
+
+private EditText fullName;
+    private EditText Email;
+    private EditText Dob;
+    private EditText phnNum;
+private Button save;
+FirebaseDatabase rootNode;
+DatabaseReference reference;
+private FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
         back = findViewById(R.id.backBTN);
+
+
+        fullName=(EditText) findViewById(R.id.editFullName);
+        Email=(EditText)findViewById(R.id.editEmail);
+        phnNum =(EditText) findViewById(R.id.editPhnNum);
+        Dob=(EditText)findViewById(R.id.editDOB);
+        save=findViewById(R.id.saveBTN);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                user = FirebaseAuth.getInstance().getCurrentUser();
+                rootNode = FirebaseDatabase.getInstance();
+             //   reference = rootNode.getReference("user");/////
+                reference = rootNode.getReference().child(user.getUid());
+
+                String name = fullName.getText().toString();
+                String email = Email.getText().toString();
+                String phn = phnNum.getText().toString();
+                String dob = Dob.getText().toString();
+                userInfo userInfo = new userInfo(name,email,phn,dob);
+
+                reference.child(phn).setValue(userInfo);
+                Toast.makeText(getApplicationContext(), "Saved Successfully", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
---
+++ app/src/main/java/com/example/karu_android_app/userInfo.java
@@ -0,0 +1,47 @@
+package com.example.karu_android_app;
+
+public class userInfo {
+    String fullName,Email,phnNum,Dob;
+
+    public userInfo() {
+    }
+
+    public userInfo(String fullName, String email, String phnNum, String dob) {
+        this.fullName = fullName;
+        Email = email;
+        this.phnNum = phnNum;
+        Dob = dob;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getPhnNum() {
+        return phnNum;
+    }
+
+    public void setPhnNum(String phnNum) {
+        this.phnNum = phnNum;
+    }
+
+    public String getDob() {
+        return Dob;
+    }
+
+    public void setDob(String dob) {
+        Dob = dob;
+    }
+}
