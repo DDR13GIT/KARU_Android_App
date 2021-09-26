0a373f08c64c655d7b995f0d2723c80db7b0cc9c
DDR13GIT <ddroy13@gmail.com>
2021-09-27 02:04:20 +0600

    picture added in database and buy Art, sell art

--- app/src/main/java/com/example/karu_android_app/PostAdapter.java
+++ app/src/main/java/com/example/karu_android_app/PostAdapter.java
@@ -3,6 +3,7 @@ package com.example.karu_android_app;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -10,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.squareup.picasso.Picasso;
 
 public class PostAdapter extends FirestoreRecyclerAdapter<postDataModel, PostAdapter.PostHolder> {
 
@@ -20,8 +22,9 @@ public class PostAdapter extends FirestoreRecyclerAdapter<postDataModel, PostAda
     @Override
     protected void onBindViewHolder(@NonNull PostHolder holder, int position, @NonNull postDataModel model) {
     holder.title.setText(model.getTitle());
-    holder.price.setText("BDT " +String.valueOf(model.getPrice())+ " ৳");
+    holder.price.setText("BDT " + String.valueOf(model.getPrice())+ " ৳");
     holder.category.setText(model.getCategory());
+    Picasso.get().load(model.getImageUrl()).into(holder.image);
     }
 
     @NonNull
@@ -35,12 +38,14 @@ public class PostAdapter extends FirestoreRecyclerAdapter<postDataModel, PostAda
         TextView title;
         TextView price;
         TextView category;
+        ImageView image;
 
         public PostHolder(@NonNull View itemView) {
             super(itemView);
             title = itemView.findViewById(R.id.postTitle);
             price = itemView.findViewById(R.id.postPrice);
             category = itemView.findViewById(R.id.postCategory);
+            image = itemView.findViewById(R.id.imageFromDatabase);
         }
     }
 }
