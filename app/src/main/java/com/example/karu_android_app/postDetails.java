d5920aea4120898e44af3906e5f352beeea34d34
DDR13GIT <ddroy13@gmail.com>
2021-09-29 03:01:38 +0600

    post details design updated -- (deb)

--- app/src/main/java/com/example/karu_android_app/postDetails.java
+++ app/src/main/java/com/example/karu_android_app/postDetails.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.travijuu.numberpicker.library.NumberPicker;
+
 public class postDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_details);
+
+        NumberPicker numberPicker = (NumberPicker) findViewById(R.id.number_picker);
+        numberPicker.setMax(15);
+        numberPicker.setMin(5);
+        numberPicker.setUnit(2);
+        numberPicker.setValue(10);
+
+        int count = numberPicker.getValue();
+
     }
 }
