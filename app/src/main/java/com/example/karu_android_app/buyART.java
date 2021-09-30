a02789855b96b2e3d3cb03e27a8b818b1a37f7f5
DDR13GIT <ddroy13@gmail.com>
2021-10-01 04:21:43 +0600

    post to onclicklistener added, post details design changed, post adapter will not be used from now

--- app/src/main/java/com/example/karu_android_app/buyART.java
+++ app/src/main/java/com/example/karu_android_app/buyART.java
@@ -2,24 +2,32 @@ package com.example.karu_android_app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.squareup.picasso.Picasso;
 
 public class buyART extends AppCompatActivity {
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private CollectionReference postReference = db.collection("Posts");
-
-    private PostAdapter adapter;
+    private curatedPostAdapter c_adapter;
+//    private PostAdapter adapter;
     private Button search_button;
-
+        FirestoreRecyclerAdapter<postDataModel,postHolder> recyclerAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,30 +41,71 @@ public class buyART extends AppCompatActivity {
             }
         });
 
-        setUpRecyclerView();
-    }
-
-    private void setUpRecyclerView() {
         Query query = postReference.orderBy("price", Query.Direction.ASCENDING);
-        FirestoreRecyclerOptions<postDataModel> options = new FirestoreRecyclerOptions.Builder<postDataModel>()
-                .setQuery(query, postDataModel.class)
-                .build();
-        adapter = new PostAdapter(options);
+        FirestoreRecyclerOptions<postDataModel> allinfo =new  FirestoreRecyclerOptions.Builder<postDataModel>().setQuery(query,postDataModel.class).build();
+        recyclerAdapter = new FirestoreRecyclerAdapter<postDataModel, postHolder>(allinfo) {
+            @Override
+            protected void onBindViewHolder(@NonNull postHolder holder, int position, @NonNull postDataModel model) {
+
+               String docId =  recyclerAdapter.getSnapshots().getSnapshot(position).getId();
+                holder.title.setText(model.getTitle());
+                holder.price.setText("BDT " + String.valueOf(model.getPrice())+ " ৳");
+                holder.category.setText(model.getCategory());
+                Picasso.get().load(model.getImageUrl()).into(holder.image);
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(getApplicationContext(),postDetails.class);
+                        intent.putExtra("title",model.getTitle());
+                        intent.putExtra("price",String.valueOf(model.getPrice()));
+                        intent.putExtra("image_uri",model.getImageUrl());
+                        intent.putExtra("description",model.getDescription());
+                        startActivity(intent);
+                    }
+                });
+            }
+
+            @NonNull
+            @Override
+            public postHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+                View view = layoutInflater.inflate(R.layout.postitem,parent,false);
+                return new postHolder(view);
+            }
+        };
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(adapter);
+        recyclerView.setAdapter(recyclerAdapter);
+
+
+    }
+
+    public class postHolder extends RecyclerView.ViewHolder{
+        TextView title;
+        TextView price;
+        TextView category;
+        ImageView image;
+
+        public postHolder(@NonNull View itemView){
+            super(itemView);
+            title = itemView.findViewById(R.id.postTitle);
+            price = itemView.findViewById(R.id.postPrice);
+            category = itemView.findViewById(R.id.postCategory);
+            image = itemView.findViewById(R.id.imageFromDatabase);
+
+        }
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        adapter.startListening();
+        recyclerAdapter.startListening();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        adapter.stopListening();
+        recyclerAdapter.stopListening();
     }
 }
--- app/src/main/java/com/example/karu_android_app/postDetails.java
+++ app/src/main/java/com/example/karu_android_app/postDetails.java
@@ -1,12 +1,19 @@
 package com.example.karu_android_app;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+import com.squareup.picasso.Picasso;
 import com.travijuu.numberpicker.library.NumberPicker;
 
 public class postDetails extends AppCompatActivity {
+
+    TextView title_view, price_view, description_view;
+    ImageView image_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +24,27 @@ public class postDetails extends AppCompatActivity {
         numberPicker.setMax(15);
         numberPicker.setMin(5);
         numberPicker.setUnit(2);
-        numberPicker.setValue(10);
+        numberPicker.setValue(1);
 
         int count = numberPicker.getValue();
+
+
+        Intent data = getIntent();
+        String title = data.getStringExtra("title");
+        String price = data.getStringExtra("price");
+        String description = data.getStringExtra("description");
+
+        String image_uri = data.getStringExtra("image_uri");
+
+        title_view = findViewById(R.id.title_details);
+        price_view = findViewById(R.id.details_price);
+        description_view = findViewById(R.id.description_details);
+        image_view = findViewById(R.id.imageViewDetails);
+
+        title_view.setText(title);
+        price_view.setText("BDT " + price + " ৳");
+        description_view.setText(description);
+        Picasso.get().load(image_uri).into(image_view);
 
     }
 }
