adcbbd985909023f735b880ab04ffeb388bf42be
Maisha2510 <maisha2510@gmail.com>
2021-09-30 23:14:58 +0600

    make interface

---
+++ app/src/main/java/com/example/karu_android_app/Interface/subCategoryOnClickInterface.java
@@ -0,0 +1,7 @@
+package com.example.karu_android_app.Interface;
+
+import android.view.View;
+
+public interface subCategoryOnClickInterface {
+    void onClick(View view,boolean isLongPressed);
+}
--- app/src/main/java/com/example/karu_android_app/postDetails.java
+++ app/src/main/java/com/example/karu_android_app/postDetails.java
@@ -3,23 +3,30 @@ package com.example.karu_android_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.travijuu.numberpicker.library.NumberPicker;
+//import com.travijuu.numberpicker.library.NumberPicker;
 
 public class postDetails extends AppCompatActivity {
+    private TextView title,price;
+    private ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_details);
 
-        NumberPicker numberPicker = (NumberPicker) findViewById(R.id.number_picker);
+     /*   NumberPicker numberPicker = (NumberPicker) findViewById(R.id.number_picker);
         numberPicker.setMax(15);
         numberPicker.setMin(5);
         numberPicker.setUnit(2);
         numberPicker.setValue(10);
+        int count = numberPicker.getValue();*/
 
-        int count = numberPicker.getValue();
+        title= findViewById(R.id.postTitle);
+        price= findViewById(R.id.postPrice);
+        image= findViewById(R.id.postImage);
 
     }
 }
